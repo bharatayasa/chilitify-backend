b5c1da13ec51284edbafdc21e28e7d40a275360f
bharatayasa <bharatayasa40@gmail.com>
2024-07-29 22:49:45 +0800

    connection improve

--- config/db.js
+++ config/db.js
@@ -2,19 +2,35 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host    : process.env.DB_HOST,
-    user    : process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+let connection;
 
-connection.connect((error) => {
-    if (error) {
-        console.error("Failed connecting to MySQL database:", error.message);
-        return;
-    }
-    console.log("Connected to MySQL database");
-});
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host    : process.env.DB_HOST,
+        user    : process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.error("Failed connecting to MySQL database:", error.message);
+            setTimeout(handleDisconnect, 2000);
+        } else {
+            console.log("Connected to MySQL database");
+        }
+    });
+
+    connection.on('error', (err) => {
+        console.error('Database connection error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = connection;
